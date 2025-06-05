a06d3d230ecb52cf0777931599a6ff32530d1156
PACOCA46678 <pacoca854@gmail.com>
2025-06-05 16:40:18 -0300

    adicionei arquivos via upload

---
+++ HelloGitHub/src/br/comgabrieldias/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.comgabrieldias.git;
+
+/**
+ * Exemplo de integração do eclipse com o github
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
